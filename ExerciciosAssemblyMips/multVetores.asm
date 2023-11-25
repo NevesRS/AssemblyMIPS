484804d35c63faadbd47832096b604793c2f509c
NevesRS <bneves.dev@gmail.com>
2023-11-25 15:01:39 -0300

    Criando codigo multiplica vetores

--- ExerciciosAssemblyMips/multVetores.asm
+++ ExerciciosAssemblyMips/multVetores.asm
@@ -1,15 +1,15 @@
 .data
-i: 		.word 0
-lim:		.word 7
-vetor1: 		.word 9 8 3 5 14 60 25 13
-vetor2: 		.word 5 4 3 2 1 10 12 11
-vetorResultado: 	.word 0 0 0 0 0 0 0 0
-multiplicador: 		.word 0
-		#ESPERADOS:  14 12 6 7 15 70 37 24
+zero: 		.word 0
+indiceMult: 	.word 0
+i:             .word 0
+lim:           .word 7
+vetorA:        .word 2 3 4 5 6 7 8 9
+vetorB:        .word 2 2 2 2 2 2 2 2
+vetorResultado: .word 0 0 0 0 0 0 0 0
+	# ESPERADOS: 4 6 8 10 12 14 16 18
 
 .text
 	.globl main
-
 main:
 	#Zerando os registradores
 	xor $t0, $t0, $t0 
@@ -17,9 +17,6 @@ main:
 	xor $t2, $t2, $t2
 	xor $t3, $t3, $t4
 	xor $t5, $t5, $t5
-	xor $t6, $t6, $t6
-	xor $t7, $t7, $t7
-	xor $t8, $t8, $t8
 	
 	la $t0, i		#Iniciando variavel de indice (Load adress)
 	lw $t0, 0($t0)		#Armazenando variável de índice no registrador (Load word)
@@ -27,11 +24,11 @@ main:
 	la $t1, lim		#Iniciando variavel de limite de indice
 	lw $t1, 0($t1) 		#Armazenando variavel de limite de indice no registrador
 	
-	la $t2, vetor1 		#Iniciando variavel de vetor1
+	la $t2, vetorA 		#Iniciando variavel de vetor1
 	la $t3, 0($t2) 		#Iniciando primeiro valor do vetor1
 	lw $t3, 0($t3) 		#Armazenando o primeiro valor do vetor1
 	
-	la $t5, vetor2		#Iniciando variavel de vetor2
+	la $t5, vetorB		#Iniciando variavel de vetor2
 	la $t6, 0($t5)		#Iniciando primeiro valor do vetor2
 	lw $t6, 0($t6) 		#Armazenando o primeiro valor do vetor2
 	
@@ -43,9 +40,13 @@ main:
 		# UMA NOVA INICIALIZAÇÃO DE REGISTRADOR
 		# UM NOVO REGISTRADOR PARA ARMAZENA O INICIO DA VARIAVEL
 		# ARMAZENAR O NOVO PRIMEIRO VALOR
-	j multVetor #Jump pro somaVetor
 	
+	bgtz $t6, multiplicacaoPos 	#Comparando se valor de $t6 > 0, se for realize multiplicacao de positivos (Sucessão de somas)
+	bltz $t6, multiplicacaoNeg	#Comparando se valor de $t6 < 0, se for realize multiplicacao de positivos (Sucessão de subtracoes)
+					#Importante notar que o valor que vai denotar o sinal da operação é o multiplicador, por isso a comparação por ele
+
 varreVetor:
+	bge $t0, $t1, end
     	addi $t0, $t0, 1 	#Incrementando o indice
     	
     	addi $t2, $t2, 4 	#Incrementando o endereço para varrer o vetor1
@@ -53,15 +54,43 @@ varreVetor:
     	
     	addi $t5, $t5, 4 	#Incrementando o endereço para varrer o vetor2
     	lw $t6, 0($t5) 		#Carregando a informacao do endereço e armazenando em $t6 	(RESPONSAVEL VETOR2)
-    	
+ 
+    	addi $t7, $t7, 4 	#Incrementando o endereço para varrer o vetorResultado
+    	lw $t8, 0($t7) 		#Carregando a informacao do endereço e armazenando em $t8 	(RESPONSAVEL VetorResultado)
+    	  
+    	bgtz $t6, multiplicacaoPos 	#Comparando se valor de $t6 > 0, se for realize multiplicacao de positivos (Sucessão de somas)
+	bltz $t6, multiplicacaoNeg	#Comparando se valor de $t6 < 0, se for realize multiplicacao de positivos (Sucessão de subtracoes)
+					#Importante notar que o valor que vai denotar o sinal da operação é o multiplicador, por isso a comparação por ele
+					
     	#PARA CADA NOVO VETOR EU PRECISO NO LOOP DE:
     		#UM NOVO INCREMENTADOR
     		#UM NOVO LW PARA CARREGAR A POSICAO DO VETOR
-    	blt $t0, $t1, multVetor #Comparando se o indice e menor que o limite de indice
+    	#blt $t0, $t1, somaVetor #Comparando se o indice e menor que o limite de indice
+    	
+    	#bgtz $t6, multiplicacaoPos 	#Comparando se valor de $t1 > 0, se for realize multiplicacao de positivos (Sucessão de somas)
+	#bltz $t6, multiplicacaoNeg	#Comparando se valor de $t1 < 0, se for realize multiplicacao de positivos (Sucessão de subtracoes)
+					#Importante notar que o valor que vai denotar o sinal da operação é o multiplicador, por isso a comparação por ele
 
-multVetor:
-    
- 
-iteracaoMult:
+multiplicacaoNeg: 
+	subi $t4, $t4, 1		#Subtraindo o indice
+	sub $t9, $t9, $t3 		#Subtraindo o multiplicando por ele mesmo
+	bgt $t4, $t6, multiplicacaoNeg	#Comparando se o indice e maior que o valor 1 se for rechama o loop
+	sw $t9, 0($t7)
+	la $t4, indiceMult
+	lw $t4, 0($t4)
+	la $t9, zero
+	lw $t9, 0($t9)
+	j varreVetor
 
+multiplicacaoPos:
+	addi $t4, $t4, 1		#Somando o indice 
+	add $t9, $t9, $t3		#Somando o multiplicando por ele mesmo
+	blt $t4, $t6, multiplicacaoPos	#Comparando se o indice e menor que o valor 1 se for rechama o loop
+	sw $t9, 0($t7)
+	la $t4, indiceMult
+	lw $t4, 0($t4)
+	la $t9, zero
+	lw $t9, 0($t9)
+	j varreVetor				
 	
+end: 
--- ExerciciosAssemblyMips/multiplicacao.asm
+++ ExerciciosAssemblyMips/multiplicacao.asm
@@ -1,13 +1,39 @@
 .data
-maior: 	.word 0
-n:	.word 7
-vetor: 	.word 9 8 3 5 14 60 25 13
-
+i:	.word 0
+valor1: 	.word 9
+valor2: 	.word 3
 .text
 	.globl main
 
 main:
+	#zerando os registradores
+	xor $t0, $t0, $t0 
+	xor $t1, $t1, $t1
+	xor $t2, $t2, $t2
+	xor $t3, $t3, $t4
 	
+	la $t0, valor1 			#Iniciando variavel de valor1
+	lw $t0, 0($t0) 			#Armazenando variável de valor1 no registrador
 
-multiplicacao:
+	la $t1, valor2 			#Iniciando variavel de valor1
+	lw $t1, 0($t1) 			#Armazenando variável de valor1 no registrador
 	
+	la $t3, i			#Iniciando variavel de indice
+	lw $t3, 0($t3)			#Armazenando variável de indice no registrador
+	
+	bgtz $t1, multiplicacaoPos 	#Comparando se valor de $t1 > 0, se for realize multiplicacao de positivos (Sucessão de somas)
+	bltz $t1, multiplicacaoNeg	#Comparando se valor de $t1 < 0, se for realize multiplicacao de positivos (Sucessão de subtracoes)
+					#Importante notar que o valor que vai denotar o sinal da operação é o multiplicador, por isso a comparação por ele
+multiplicacaoNeg: 
+	subi $t3, $t3, 1		#Subtraindo o indice
+	sub $t2, $t2, $t0 		#Subtraindo o multiplicando por ele mesmo
+	bgt $t3, $t1, multiplicacaoNeg	#Comparando se o indice e maior que o valor 1 se for rechama o loop
+	j end				#Pulando para o fim
+
+multiplicacaoPos:
+	addi $t3, $t3, 1		#Somando o indice 
+	add $t2, $t2, $t0 		#Somando o multiplicando por ele mesmo
+	blt $t3, $t1, multiplicacaoPos	#Comparando se o indice e menor que o valor 1 se for rechama o loop
+	j end				
+
+end: 
